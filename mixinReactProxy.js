aa9217668d7b89704aa334cdda1cc105a3cce2d2
Minh Tran <vn38minhtran@gmail.com>
2015-11-30 10:33:01 +0700

    remove .props.ref of the React component <ReactProxy />

--- mixinReactProxy.js
+++ mixinReactProxy.js
@@ -1,28 +1,25 @@
-module.exports = function(React, desc) {
-	desc.displayName = "ReactProxy";
-	desc.componentRefName = 'component';
-	desc.render = function() {
-		var Component = this.state.component;
-		if(Component) {
-			var props = this.props;
-			props.ref = desc.componentRefName;
-			return React.createElement(Component, props, this.props.children);
-		} else if(this.renderUnavailable) {
-			return this.renderUnavailable();
-		} else {
-			return null;
-		}
-	};
-	desc.getInitialState = function() {
-		return { component: this.loadComponent() };
-	};
-	desc.componentDidMount = function() {
-		if(!this.state.component) {
-			this.loadComponent(function(component) {
-				if(this.isMounted()) {
-					this.setState({ component: component });
-				}
-			}.bind(this));
-		}
-	};
+module.exports = function (React, desc) {
+  desc.displayName = "ReactProxy";
+  desc.render = function () {
+    var Component = this.state.component;
+    if (Component) {
+      return React.createElement(Component, this.props, this.props.children);
+    } else if (this.renderUnavailable) {
+      return this.renderUnavailable();
+    } else {
+      return null;
+    }
+  };
+  desc.getInitialState = function () {
+    return {component: this.loadComponent()};
+  };
+  desc.componentDidMount = function () {
+    if (!this.state.component) {
+      this.loadComponent(function (component) {
+        if (this.isMounted()) {
+          this.setState({component: component});
+        }
+      }.bind(this));
+    }
+  };
 };
